73cb1fbaa3ebf41917ac84664450d861b56e4db3
Unknown <Vilius.vaiciunas12@gmail.com>
2018-04-05 11:53:33 +0300

    Made migration and seeder for channels table

--- api/database/seeds/UsersTableSeeder.php
+++ api/database/seeds/UsersTableSeeder.php
@@ -11,8 +11,17 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
-        $u = factory(App\User::class, 50)->make();
+        //The line below should use factory to seed but for some reason doesn't work
+        //$u = factory(App\User::class, 50)->make();
+
+        //this is a ghetto manual "factory"
+        for($i = 0; $i < 10; $i++) {
+            DB::table('users')->insert([
+                'name' => str_random(5),
+                'email' => str_random(10) . '@gmail.com',
+                'password' => "password",
+            ]);
+        }
 
     }
 }
