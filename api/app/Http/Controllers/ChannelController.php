7534b5920a609c845c3bb55176ce8305c3669819
Unknown <Vilius.vaiciunas12@gmail.com>
2018-04-20 12:38:16 +0300

    Finished Stream Key generation and authorization

--- api/app/Http/Controllers/ChannelController.php
+++ api/app/Http/Controllers/ChannelController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Channel;
+use App\Channel as Channel;
 use App\Http\Resources\Channels;
+use Illuminate\Support\Facades\Auth;
+use App\User as User;
+use Symfony\Component\HttpFoundation\Response;
 
 class ChannelController extends Controller
 {
@@ -13,10 +16,45 @@ class ChannelController extends Controller
     }
 
     public function getStreamKey(){
+        $user = Auth::user();
+
+        if(Channel::where('id', $user->id)->count() >= 1){
+            //UPDATE
+            $channel = Channel::find($user->id);
+
+            $key = md5(microtime().rand());
+            $channel->name = $key;
+
+            $channel->save();
+            return $key;
+        } else {
+            //INSERT
+            $channel = new Channel;
+            $channel->id = $user->id;
+
+            $key = md5(microtime().rand());
+            $channel->name = $key;
+
+            $channel->save();
+            return $key;
+        }
 
     }
 
     public function authStream(Request $request){
-        error_log($request);
+        $username = $request['user'];
+        $password = $request['pass'];
+        $mount = ltrim($request['mount'],'/');
+
+        if(User::where('name', $username)->first() != null && $mount == $username){
+            $id = User::select('id')->where('name', $username)->first()->id;
+            error_log($id);
+            $key = Channel::select('name')->where('id', $id)->first()->name;
+            if($key == $password){
+                return response() -> json() -> header('icecast-auth-user','1');
+            }
+        }
+
+        return response() -> json() -> header('icecast-auth-user','0');
     }
 }
